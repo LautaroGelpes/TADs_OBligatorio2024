bbc066e615d5f2daff5f8f942d6646c15dc289da
LIMoreno04 <ignamoreno04@gmail.com>
2024-05-22 21:06:33 -0300

    Agregué HashEncadenado.

---
+++ src/uy/edu/um/adt/hash/HashEncadenado.java
@@ -0,0 +1,89 @@
+package uy.edu.um.adt.hash;
+
+import uy.edu.um.adt.Exceptions.InvalidValue;
+import uy.edu.um.adt.linkedlist.MyLinkedListImpl;
+public class HashEncadenado<K,V> implements MyHashTable<K,V>{
+    private MyLinkedListImpl<ValueStash<K,V>>[] hashTable;
+    private int size;
+
+    public HashEncadenado() {
+        this.size = 11;
+        this.hashTable = new MyLinkedListImpl[size];
+    }
+
+    public HashEncadenado(int size) {
+        this.size = size;
+        this.hashTable = new MyLinkedListImpl[size];
+    }
+
+    //metodo auxiliar para encontrar el stash directo en vez del valor
+    private ValueStash<K,V> findStash(K key) throws InvalidValue {
+        if (hashTable[hashFunction(key)] != null) {         //si hay un elemento en la posición correspondiente
+            MyLinkedListImpl<ValueStash<K, V>> listaColisiones = hashTable[hashFunction(key)];  //me guardo la linked list que contendría el valor
+            for (int i = 0; i < listaColisiones.size(); i++) {
+                if (listaColisiones.get(i).getKey().equals(key)) {      //busco en la linked list
+                    return listaColisiones.get(i);           //si lo encuentro, retorno el valor
+                }
+            }
+        }//si no se encuentra o si la posición no tenía nada
+        throw new InvalidValue();
+    }
+    private int hashFunction(K key){
+        String sKey = key.toString();
+        int hashedKey = 0;
+        for (int i = 0; i < sKey.length(); i++) {
+            char aux = sKey.charAt(i);
+            hashedKey += (int) aux;
+        }
+        return hashedKey % size;
+    }
+
+    @Override
+    public void put(K key, V value) {
+        ValueStash<K,V> stash = new ValueStash<>();
+        stash.setKey(key);
+        stash.setValue(value);
+        hashTable[hashFunction(key)].add(stash);
+    }
+    public V find(K key) throws InvalidValue {
+        return findStash(key).getValue();
+    }
+
+    @Override
+    public boolean contains(K key) {
+        if (hashTable[hashFunction(key)] != null) { //si hay elementos en la posición correspondiente
+            ValueStash<K,V> aux = new ValueStash<>();
+            aux.setKey(key); //creo un ValueStash para comparar, (el .equals() solo ve la key)
+
+            return hashTable[hashFunction(key)].contains(aux); //uso el método de las LinkedList
+        }
+        //no hay elementos en la posición
+        return false;
+    }
+
+    @Override
+    public void remove(K clave) throws InvalidValue{
+        if (hashTable[hashFunction(clave)] != null){ //si hay elementos en la posición correspondiente
+            ValueStash<K,V> aux = new ValueStash<>();
+            aux.setKey(clave); //creo un ValueStash para comparar, (el .equals() solo ve la key)
+            hashTable[hashFunction(clave)].remove(aux); //uso el método de las LinkedList
+        }
+        //no hay elementos en la posición
+        throw new InvalidValue();
+    }
+
+    public void resize(int newSize){
+        MyLinkedListImpl<ValueStash<K,V>>[] newHashTable = new MyLinkedListImpl[newSize];
+        this.size = newSize;
+        for (int i = 0; i < this.size; i++) {
+            if (this.hashTable[i] != null) {
+                for (int j = 0; j < this.hashTable[i].size(); j++) {
+                    ValueStash<K,V> aux = this.hashTable[i].get(j);
+                    newHashTable[hashFunction(aux.getKey())].add(aux);
+                }
+            }
+        }
+        this.hashTable = newHashTable;
+    }
+
+}
